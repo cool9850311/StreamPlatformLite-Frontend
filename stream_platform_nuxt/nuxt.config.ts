0b62ed3724ffa97b7708ac12819f7750da078659
Harry Shih <cool9850311cool@gmail.com>
2025-02-16 19:20:40 +0800

    feat: integrate download record api

--- stream_platform_nuxt/nuxt.config.ts
+++ stream_platform_nuxt/nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-
+  sourcemap: {
+    server: true,
+    client: true
+  },
   runtimeConfig: {
     public: {
       DISCORD_URL: '',
